4e7cefcd7f5b659ba090bc4e5dc46bd849ab6c13
xf <xf@qq.com>
2022-09-23 10:37:08 +0800

    括号生成

---
+++ 22.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+void create_str(char ** str, char *temp, int n, int *returnSize, int left, int right) {
+    static int k = 0;
+
+    if (temp[2 * n - 1] == '(' || temp[2 * n - 1] == ')') {
+        strcpy_s(str[*returnSize], 2*n+1, temp);
+        (* returnSize)++;
+        return;
+    }
+
+    if (left < n) {
+        temp[k] = '(';
+        k++;
+        create_str(str, temp, n, returnSize, left+1, right);
+        temp[--k] = ' ';
+    }
+
+    if (right < left) {
+        temp[k] = ')';
+        k++;
+        create_str(str, temp, n, returnSize, left, right+1);
+        temp[--k] = ' ';
+    }
+    return;
+}
+
+char** generateParenthesis(int n, int* returnSize) {
+    (*returnSize) = 0;
+    char** str = (char**)malloc(sizeof(char*) * (int)pow(2, 2 * (int)n));
+    if (str) {
+        for (int i = 0; i < pow(2, n * 2); i++) {
+            str[i] = (char*)malloc(sizeof(char) * (2 * n));
+            if (!str[i]) {
+                break;
+            }
+            memset(str[i], 0, sizeof(char) * (2 * n));
+        }
+    }
+
+
+    char* temp = (char*)malloc(sizeof(char) * (2 * n + 1));
+    if (temp) {
+        temp[2 * n] = '\0';
+    }
+
+    create_str(str, temp, n, returnSize, 0, 0);
+
+    return str;
+}
+
+void code_22() {
+    int returnSize = -1;
+    int n = 3;
+    char** ret = generateParenthesis(n, &returnSize);
+
+    for (int i = 0; i < returnSize; i++) {
+
+        printf("ret=%s\n", ret[i]);
+    }
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void code_22();
+
+#endif // !_MAIN_H_
