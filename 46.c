7d092dc09769641d3f8da9bbdb037c95b71819dc
xf <xf@qq.com>
2022-09-23 18:10:40 +0800

    全排列

--- 46.c
+++ 46.c
@@ -1,3 +1,7 @@
+/*
+给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+*/
+
 #include "main.h"
 
 #define NUMSSIZE 3
@@ -16,15 +20,30 @@ int factorial(int n) {
 
 int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
 	*returnSize = factorial(numsSize);
-	returnColumnSizes = (int**)malloc(sizeof(int*) * (*returnSize));
-	printf("returnColumnSizes = %d\n", returnColumnSizes);
+	printf("%d\n", *returnSize);
+	int ** result = (int**)malloc(sizeof(int*) * (*returnSize));
+	if (!result) {
+		return;
+	}
+	for (int i = 0; i < numsSize; i++) {
+		result[i] = (int*)malloc(sizeof(int) * numsSize);
+	}
+	for (int i = 0; i < *returnSize; i++) {
+		for (int j = 0; j < numsSize; j++) {
+			result[i][j] = 0;
+			printf("%d ", result[i][j]);
+		}
+		printf("\n");
+	}
 
 }
 
 
 void code_46() {
 	int nums[NUMSSIZE] = { 1,2,3 };
-	int returnSize = 0;
+
 	int** returnColumnSizes;
+	int returnSize = factorial(NUMSSIZE);
+	returnColumnSizes = (int**)malloc(sizeof(int*) * (returnSize));
 	returnColumnSizes =  permute(nums, NUMSSIZE, &returnSize, returnColumnSizes);
 }
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
 int main() {
-	code_22();
+	//code_22();
+	code_46();
 }
