2dad656ea473df5dd23dd69d1550293b646b9e0a
xf <xf@qq.com>
2022-10-09 16:46:26 +0800

    2022-10-09

--- 18.c
+++ 18.c
@@ -9,6 +9,8 @@ nums[a] + nums[b] + nums[c] + nums[d] == target
 输入：nums = [1,0,-1,0,-2,2], target = 0
 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
 
+* @Author	:	xiefei
+* @Date		:	2000-10-09
 */
 
 #include "main.h"
@@ -19,10 +21,79 @@ nums[a] + nums[b] + nums[c] + nums[d] == target
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 
-int** fourSum(int* nums, int numsSize, int target, int* returnSize, int** returnColumnSizes) {
-
+void sort_18(int *nums, int numsSize) {
+	for (int i = 0; i < numsSize - 1; i++) {
+		for (int j = i + 1; j < numsSize; j++) {
+			if (nums[i] > nums[j]) {
+				int temp = nums[i];
+				nums[i] = nums[j];
+				nums[j] = temp;
+			}
+		}
+	}
 }
 
-void code_18() {
+int** fourSum(int* nums, int numsSize, int target, int* returnSize, int** returnColumnSizes) {
 
+	int** result = (int**)malloc(sizeof(int*) * 2000);
+	if (*result) {
+		for (int i = 0; i < 2000; i++) {
+			result[i] = (int*)malloc(sizeof(int) * 4);
+			memset(result[i], 0, 4);
+		}
+	}
+
+	sort_18(nums, numsSize);
+	for (int i = 0; i < numsSize - 3; i++) {
+		for (int j = i + 1; j < numsSize - 2; j++) {
+			int left = j + 1;
+			int right = numsSize - 1;
+			while (left < right) {
+				int sum = nums[i] + nums[j] + nums[left] + nums[right];
+				if (sum < target) {
+					left++;
+				}
+				else if (sum > target) {
+					right--;
+				}
+				else {
+					(returnColumnSizes)[*returnSize] = 4;
+					//printf("%d %d %d %d\n", nums[i], nums[j], nums[left], nums[right]);
+					(*result + (*returnSize))[0] = nums[i];
+					(*result + (*returnSize))[1] = nums[j];
+					(*result + (*returnSize))[2] = nums[left];
+					(*result + (*returnSize))[3] = nums[right];
+					for (int w = 0; w < 4; w++) {
+						printf("%d ", (*result + (*returnSize))[w]);
+					}
+					printf("returnSize = %d  size = %d\n", *returnSize, returnColumnSizes[*returnSize]);
+					(*returnSize)++;
+					break;
+				}
+			}
+		}
+	}
+	printf("size = %d\n", *returnSize);
+	for (int i = 0; i < *returnSize; i++) {
+		printf("returnSize = %d  size = %d\n",i , returnColumnSizes[i]);
+	}
+	return result;
+}
+
+#define numsSize  6
+void code_18() {
+	int nums[numsSize] = { 1,0,-1,0,-2,2 };
+	int target = 0;
+	int returnSize = 0;
+	int* returnColumnSize = (int *)malloc(sizeof(int) * 2000);
+	int **ret = fourSum(nums, numsSize, target, &returnSize, returnColumnSize);
+	for (int i = 0; i < returnSize; i++) {
+		printf("size = %d \n", returnColumnSize[i]);
+	}
+	for (int i = 0; i < returnSize; i++) {
+		for (int j = 0; j < returnColumnSize[i]; j++) {
+			printf("%d ", ret[i][j]);
+		}
+		printf("\n");
+	}
 }
--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 int main() {
-	// code_8();
-	// code_12();
-	// code_17();
-	// code_18();
-	// code_22();
-	// code_46();
-	// code_811();
-	code_856();
-	// code_870();
-	// code_921();
+	//code_8();
+	//code_12();
+	//code_17();
+	code_18();
+	//code_22();
+	//code_46();
+	//code_811();
+	//code_856();
+	//code_870();
+	//code_921();
 }
