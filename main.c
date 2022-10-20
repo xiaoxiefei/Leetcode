7fa966747288d6a641df9d6094bcc43b89df0949
xf <2296115288@qq.com>
2022-10-20 21:03:19 +0800

    2022-10-20

--- main.c
+++ main.c
@@ -4,9 +4,10 @@ int main() {
 	//code_8();
 	//code_12();
 	//code_17();
-	code_18();
+	//code_18();	//	unsolved
 	//code_22();
 	//code_46();
+	code_779();
 	//code_811();
 	//code_856();
 	//code_870();
